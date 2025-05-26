3bfdc737fe5432502d704b268d951fe27f37fa17
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2025-05-26 08:43:40 +0200

    aggiunto controllo su numero di pagine

--- script.sh
+++ script.sh
@@ -5,8 +5,15 @@ if [ -f "./main.c" ] && [ -f "./files/utility.c" ]; then
     echo "file exists"
 fi
 
+echo "Inserire numero di page_frame:"
+read page_frame
+while [ $page_frame -le 0 ]; do
+    echo "Numero di pagine non valide, riprovare"
+    read page_frame
+done
+
 
 gcc ./main.c ./files/utility.c
 chmod +x a.out
 
-./a.out 4 ./inputs/
+./a.out $page_frame ./inputs/
